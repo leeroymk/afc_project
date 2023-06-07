3722c3a78cc9b425593bd625cae5317d54b0693a
leeroymk <nikekursk@yandex.ru>
2023-06-07 23:24:26 +0300

    logging added, minor refactoring, new requirements.txt

--- parsing/sports_parser.py
+++ parsing/sports_parser.py
@@ -1,8 +1,7 @@
-
-import time
+import logging
 import requests
 from random import choice
-from pprint import pprint
+
 import lxml
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -14,7 +13,6 @@ from bs4 import BeautifulSoup
 
 # Проверяем URL на возможность соединения
 def get_html(url):
-    print('Проверка соединения...')
     try:
         result = requests.get(url, headers=random_headers())
         result.raise_for_status()
@@ -26,11 +24,9 @@ def get_html(url):
 
 # Получаем список команд лиги
 def get_teams_list(html):
-    print('Получаем список команд лиги...')
     soup = BeautifulSoup(html, 'lxml')
     teams_list = soup.find_all(class_='b-tag-table__content-team')
-    teams_data = {}
-    # Создаем словарь, добавляем ссылки к ключам-названиям команды
+    # Добавляем ссылки к ключам-названиям команды
     for team in teams_list:
         teams_data.update(
             {
@@ -39,69 +35,41 @@ def get_teams_list(html):
                 }
             }
         )
-    # Наполняем словарь новостями о каждой команде лиги
-    for team in teams_list:
-        team_name = team.find('a').text
-        teams_data.update(
-            {
-                team_name: {
-                    # Парсим новости каждой команды (передаем в скроллер URL)
-                    'club_news': selenium_scroller(teams_data[team_name]['url'])
-                }
-            }
-        )
-
-    return print('Парсинг завершился успешно!')
+    return teams_list
 
 
-# Скроллим страницу на 5 прокруток
+# Скроллим страницу
 def selenium_scroller(url):
-    print(f'Открываем Selenium, скроллим страницу {url}...')
-    chrome_options = Options()
-    chrome_options.add_argument('--ignore-certificate-errors-spki-list')
-    chrome_options.add_argument('--ignore-ssl-errors')
-    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    browser = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=chrome_options,
-        )
     browser.get(url)
     try:
-        for i in range(1, 6):
-            browser.implicitly_wait(10)
+        # Здесь настраиваем количество страниц прокрутки
+        for i in range(3):
             btn_xpath = '//button[(contains(@class,"b-tag-lenta__show-more-button")) and(contains(text(),"Показать еще"))]'
             more_btn = browser.find_element(By.XPATH, btn_xpath)
             browser.execute_script("arguments[0].click();", more_btn)
-            print(f"Click # {i} done!")
-    except (requests.RequestException, ValueError):
-        print('Сетевая ошибка')
+    except (requests.RequestException, ValueError) as er:
+        print(f'Ошибка {er} при парсинге {url}')
     finally:
         # Передаем в парсер новостей прокрученную страницу
-        get_team_news(browser.page_source)
-        print("Очередной этап парсинга завершен!\n\n")
-        browser.quit()
+        return get_team_news(browser.page_source)
 
 
-# Сам процесс парсинга новостей
+# Процесс парсинга новостей
 def get_team_news(scrolled_page):
-    print('Получили страницу, парсим новости...')
     soup = BeautifulSoup(scrolled_page, 'lxml')
-
-    result_news = []
-
+    parsed_news = []
     blog_news = soup.find_all(class_='b-tag-lenta__item m-type_blog')
     for bnew in blog_news:
         time_news = ''.join(
             bnew.find(class_='b-tag-lenta__item-details').text).strip()
         title = bnew.find('h2').text
         url = bnew.find('a')['href']
-        result_news.append(
+        parsed_news.append(
             {
                 'time': time_news,
                 'title': title,
                 'url': url,
             })
-
     short_news = soup.find_all(class_='b-tag-lenta__item m-type_news')
     for snew in short_news:
         time_date = ''.join(
@@ -112,30 +80,47 @@ def get_team_news(scrolled_page):
             news_exact_time = f'{time_date}, {exact_time}'
             title = element.find('h2').text
             url = element.find('a')['href']
-            result_news.append(
+            parsed_news.append(
                 {
                     'time': news_exact_time,
                     'title': title,
-                    'url': url
+                    'url': url,
                     }
             )
-    print('Получили новости команды:\n ')
-    pprint(result_news)
-    return result_news
+    return parsed_news
 
 
 if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO,
+                        filename='log_parsing.log',
+                        format="[%(asctime)s] %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
+                        datefmt='%H:%M:%S',
+                        )
+    logging.debug("A DEBUG Message")
+    logging.info("An INFO")
+    logging.warning("A WARNING")
+    logging.error("An ERROR")
+    logging.critical("A message of CRITICAL severity")
+
     desktop_agents = [
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
-        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/114.0.5735.99 Mobile/15E148 Safari/604.1',
+        'Mozilla/5.0 (iPad; CPU OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/114.0.5735.99 Mobile/15E148 Safari/604.1',
+        'Mozilla/5.0 (iPod; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/114.0.5735.99 Mobile/15E148 Safari/604.1',
+        'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.60 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 10; SM-A205U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.60 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 10; SM-A102U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.60 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 10; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.60 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 10; SM-N960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.60 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 10; LM-Q720) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.60 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 10; LM-X420) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.60 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 10; LM-Q710(FGN)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.60 Mobile Safari/537.36',
+
         ]
 
     def random_headers():
@@ -143,8 +128,31 @@ if __name__ == "__main__":
                 'User-Agent': choice(desktop_agents),
                 }
 
-    html = get_html('https://m.sports.ru/epl/table/')
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--ignore-certificate-errors-spki-list')
+    chrome_options.add_argument('--ignore-ssl-errors')
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    browser = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=chrome_options,
+        )
+
+    # Финальный словарь с новостями по каждой команде
+    teams_data = {}
+
+    teams_table = 'https://m.sports.ru/epl/table/'
+    html = get_html(teams_table)
     if html:
-        get_teams_list(html)
+        for team in get_teams_list(html):
+            team_name = team.find('a').text
+            teams_data.update(
+                {
+                    # Парсим новости каждой команды (передаем URL в скроллер)
+                    team_name: selenium_scroller(teams_data[team_name]['url'])
+                    }
+            )
     else:
         print('Что-то пошло не по плану...')
+
+    browser.quit()
