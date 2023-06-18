5af200b46819781e8cde9123d724b7d05d98a155
Alexander Sapko <sapcoreq@gmail.com>
2023-06-18 15:50:41 +0300

    updated  view:  two teams

--- news/views.py
+++ news/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .models import News
 
-
+#  TODO: ограничить длину заголовка 197 символами + '...'. Наполнять проблеами (мб), если длина короче.
+# Создать  промежуточный словарь
 def index(request):
+    team_1 = 'Арсенал'
+    team_2 = 'Манчестер Сити'
+    qty = 5
+
     context = {
-        'news': News.objects.all()
+        'team_1': team_1,
+        'team_2': team_2,
+        'news_1':  News.objects.filter(team=team_1).order_by('-date')[:qty],
+        'news_2':  News.objects.filter(team=team_2).order_by('-date')[:qty],
     }
     return render(request, 'news/index.html', context)
