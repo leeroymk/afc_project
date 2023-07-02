1bc7b552864382352b6ec3bd1819c7bc61a0d397
Anton Sotnikov <nikekursk@gmail.com>
2023-07-02 16:59:06 +0300

    fixed tests without logging

--- fwa/tests.py
+++ fwa/tests.py
@@ -1,25 +1,5 @@
-import logging
-import sys
 from unittest import TestCase
-
 from django.test import Client
-from django.db.utils import OperationalError
-
-
-root = logging.getLogger()
-root.setLevel(logging.INFO)
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-root.addHandler(handler)
-
-logging.basicConfig(level=logging.INFO,
-                    filename='log_parsing.log',
-                    format="[%(asctime)s] %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
-                    datefmt='%H:%M:%S',
-                    )
 
 
 class TestConnection(TestCase):
@@ -28,18 +8,10 @@ class TestConnection(TestCase):
 
     def test_index_page(self):
         # Тест соединения с главной страницей
-        try:
-            response = self.client.get("/")
-            self.assertEqual(response.status_code, 200)
-        except OperationalError as er:
-            logging.error(f'Connection issues at index page: {er}')
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
 
     def test_news_page(self):
         # Тест соединения со страницей новостей
-        try:
-            response = self.client.get("/news/")
-            self.assertEqual(response.status_code, 200)
-        except OperationalError as er:
-            logging.error(f'Connection issues at news page: {er}')
-
-    logging.info('Start testing connection...')
+        response = self.client.get("/news/")
+        self.assertEqual(response.status_code, 200)
