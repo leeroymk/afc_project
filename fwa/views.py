3238e7a7d918bf9b1a649ba2b8101feab1fb9db1
Alexander Sapko <sapcoreq@gmail.com>
2023-07-05 23:01:03 +0300

    comments are implemented

--- fwa/views.py
+++ fwa/views.py
@@ -4,46 +4,40 @@ from .models import News, Teams, StatEpl, GoalscorersEPL, AssistentsEPL, Calenda
 
 def index(request):
     if request.GET:
-        team_1 = request.GET['team_name']
-        team_2 = 'Манчестер Сити'
-        qty = 5
-
-        context = {
-            'team_1': team_1,
-            'team_2': team_2,
-            'logo_1': Teams.objects.get(name=team_1).logo,
-            'logo_2': Teams.objects.get(name=team_2).logo,
-            'news_1': News.objects.filter(team=Teams.objects.get(name=team_1).id).order_by('-date')[:qty],
-            'news_2': News.objects.filter(team=Teams.objects.get(name=team_2).id).order_by('-date')[:qty],
-            'stats': StatEpl.objects.all(),
-            'stats_1': StatEpl.objects.get(team=Teams.objects.get(name=team_1).id),
-            'stats_2': StatEpl.objects.get(team=Teams.objects.get(name=team_2).id),
-            'goalscorers': GoalscorersEPL.objects.all(),
-            'assistents': AssistentsEPL.objects.all(),
-            'matches': CalendarMatches.objects.all(),
-            'getreq': f'?team_name={team_1}',
-        }
-
-        return render(request, 'fwa/index.html', context)
+        team_1 = Teams.objects.get(name=request.GET['team_name'])
     else:
-        context = {
-            'teams': Teams.objects.all().order_by('name'),
-        }
+        team_1 = Teams.objects.get(name='Арсенал')
 
-        return render(request, 'fwa/choose_team.html', context)
+    team_2 = Teams.objects.get(name='Челси')
+    qty = 5
+
+    context = {
+        'team_1': team_1,
+        'team_2': team_2,
+        'news_1': News.objects.filter(team=team_1).order_by('-date')[:qty],
+        'news_2': News.objects.filter(team=team_2).order_by('-date')[:qty],
+        'stats': StatEpl.objects.all(),
+        'stats_1': StatEpl.objects.get(team=team_1),
+        'stats_2': StatEpl.objects.get(team=team_2),
+        'goalscorers': GoalscorersEPL.objects.all(),
+        'assistents': AssistentsEPL.objects.all(),
+        'matches': CalendarMatches.objects.all(),
+        'teams': Teams.objects.all().order_by('name'),
+    }
+
+    return render(request, 'fwa/index.html', context)
 
 
 def news(request):
-    team_1 = request.GET['team_name']
-    team_2 = 'Манчестер Сити'
+    team_1 = Teams.objects.get(name=request.GET['team_name'])
+    team_2 = Teams.objects.get(name='Челси')
     qty = 25
 
     context = {
         'team_1': team_1,
         'team_2': team_2,
-        'news_1': News.objects.filter(team=Teams.objects.filter(name=team_1).first().id).order_by('-date')[:qty],
-        'news_2': News.objects.filter(team=Teams.objects.filter(name=team_2).first().id).order_by('-date')[:qty],
-        'getreq': f'?team_name={team_1}',
+        'news_1': News.objects.filter(team=team_1).order_by('-date')[:qty],
+        'news_2': News.objects.filter(team=team_2).order_by('-date')[:qty],
     }
 
     return render(request, 'fwa/news.html', context)
@@ -51,13 +45,14 @@ def news(request):
 
 def stats(request):
     season = '2022/2023'
-    team_1 = request.GET['team_name']
+    team_1 = Teams.objects.get(name=request.GET['team_name'])
+    team_2 = Teams.objects.get(name='Челси')
 
     context = {
         'season': season,
         'stats': StatEpl.objects.filter(season=season).order_by('position'),
         'team_1': team_1,
-        'getreq': f'?team_name={team_1}',
+        'team_2': team_2,
     }
 
     return render(request, 'fwa/stats.html', context)
