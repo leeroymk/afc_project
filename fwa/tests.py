9d8a8731c740f9d5e365aed773c4dd2612a6ded7
Alexander Sapko <sapcoreq@gmail.com>
2023-07-09 13:20:58 +0300

    tests are updated

--- fwa/tests.py
+++ fwa/tests.py
@@ -1,17 +1,27 @@
 from unittest import TestCase
 from django.test import Client
+from fwa.models import Teams
 
 
 class TestConnection(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_index_page(self):
-        # Тест соединения с главной страницей
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+    if Teams.objects.first():
+        def test_index_page(self):
+            for team in Teams.objects.all():
+                # Тест соединения с главной страницей
+                response = self.client.get(f"/?team_name={team.name}")
+                self.assertEqual(response.status_code, 200)
 
-    def test_news_page(self):
-        # Тест соединения со страницей новостей
-        response = self.client.get("/news/")
-        self.assertEqual(response.status_code, 200)
+        def test_news_page(self):
+            for team in Teams.objects.all():
+                # Тест соединения со страницей новостей
+                response = self.client.get(f"/news/?team_name={team.name}")
+                self.assertEqual(response.status_code, 200)
+
+        def test_stats_page(self):
+            for team in Teams.objects.all():
+                # Тест соединения со страницей новостей
+                response = self.client.get(f"/stats/?team_name={team.name}")
+                self.assertEqual(response.status_code, 200)
