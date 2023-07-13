e18895fd7904f5f7fce7cfcab3f3fcaeeb97bc88
Anton Sotnikov <nikekursk@gmail.com>
2023-07-13 13:41:20 +0300

    minor fix

--- fwa/management/commands/req_fun.py
+++ fwa/management/commands/req_fun.py
@@ -11,7 +11,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 from fwa.models import News, Teams
