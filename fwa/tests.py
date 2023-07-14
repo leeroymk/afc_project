b225c9d4437a4a8ec2542e70b78780d8d14834b4
Alexander Sapko <sapcoreq@gmail.com>
2023-07-14 22:16:05 +0300

    tests are updated

--- fwa/tests.py
+++ fwa/tests.py
@@ -26,3 +26,21 @@ class TestConnection(TestCase):
             for team in Teams.objects.all():
                 response = self.client.get(f"/stats/?team_name={team.name}")
                 self.assertEqual(response.status_code, 200)
+
+        # Тест соединения со страницей бомбардиров
+        def test_goalscorers_page(self):
+            for team in Teams.objects.all():
+                response = self.client.get(f"/goalscorers/?team_name={team.name}")
+                self.assertEqual(response.status_code, 200)
+
+        # Тест соединения со страницей статистики
+        def test_assistants_page(self):
+            for team in Teams.objects.all():
+                response = self.client.get(f"/assistants/?team_name={team.name}")
+                self.assertEqual(response.status_code, 200)
+
+        # Тест соединения со страницей статистики
+        def test_calendar_page(self):
+            for team in Teams.objects.all():
+                response = self.client.get(f"/calendar/?team_name={team.name}")
+                self.assertEqual(response.status_code, 200)
